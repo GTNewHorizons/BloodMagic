ea68e2fbb9e2be5a886c6e35e8b1ffc50c62d2a4
MuXiu1997 <muxiu1997@gmail.com>
2022-04-28 18:51:37 +0800

    Add suport of using the GT miners to mine the ores from the BloodMagic ritual "Mark of the Falling Tower"

--- src/main/java/WayofTime/alchemicalWizardry/common/summoning/meteor/MeteorParadigm.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/summoning/meteor/MeteorParadigm.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import gregtech.common.blocks.GT_TileEntity_Ores;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import WayofTime.alchemicalWizardry.AlchemicalWizardry;
 
@@ -123,7 +125,11 @@ public class MeteorParadigm
                             ItemStack blockStack = mpc.getValidBlockParadigm();
                             if(blockStack != null && blockStack.getItem() instanceof ItemBlock)
                             {
-                            	((ItemBlock)blockStack.getItem()).placeBlockAt(blockStack, null, world, x + i, y + j, z + k, 0, 0, 0, 0, blockStack.getItemDamage());
+                                ((ItemBlock)blockStack.getItem()).placeBlockAt(blockStack, null, world, x + i, y + j, z + k, 0, 0, 0, 0, blockStack.getItemDamage());
+                                final TileEntity tileEntity = world.getTileEntity(x + i, y + j, z + k);
+                                if (tileEntity instanceof GT_TileEntity_Ores) {
+                                    ((GT_TileEntity_Ores)tileEntity).mNatural = true;
+                                }
                             	world.markBlockForUpdate(x + i, y + j, z + k);
                                 hasPlacedBlock = true;
                                 break;
