0ae255a42d79785b9feb1c8164a2838f4d4c1027
Raven Szewczyk <git@eigenraven.me>
2023-07-04 21:39:47 +0100

    Only use GTNH CT APIs when GTNH CraftTweaker is present

--- src/main/java/WayofTime/alchemicalWizardry/common/tweaker/MineTweakerIntegration.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/tweaker/MineTweakerIntegration.java
@@ -1,7 +1,12 @@
 package WayofTime.alchemicalWizardry.common.tweaker;
 
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.versioning.ArtifactVersion;
+import cpw.mods.fml.common.versioning.InvalidVersionSpecificationException;
+import cpw.mods.fml.common.versioning.VersionRange;
 import minetweaker.MineTweakerAPI;
 import minetweaker.MineTweakerImplementationAPI;
+import minetweaker.mc1710.MineTweakerMod;
 import minetweaker.util.IEventHandler;
 
 /**
@@ -17,8 +22,24 @@ public class MineTweakerIntegration {
         MineTweakerAPI.registerClass(FallingTower.class);
         MineTweakerAPI.registerClass(HarvestMoon.class);
 
-        MineTweakerImplementationAPI.onRollbackEvent(new HandleLateAdditionsAndRemovals());
         MineTweakerImplementationAPI.onPostReload(new HandleLateAdditionsAndRemovals());
+        // GTNH-Only features (post 3.1.0)
+        final ArtifactVersion mtVersion = Loader.instance().getIndexedModList().get(MineTweakerMod.MODID)
+                .getProcessedVersion();
+        try {
+            if (VersionRange.createFromVersionSpec("[3.2.4,)").containsVersion(mtVersion)) {
+                GTNHIntegration.register();
+            }
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class GTNHIntegration {
+
+        public static void register() {
+            MineTweakerImplementationAPI.onRollbackEvent(new HandleLateAdditionsAndRemovals());
+        }
     }
 
     public static class HandleLateAdditionsAndRemovals
