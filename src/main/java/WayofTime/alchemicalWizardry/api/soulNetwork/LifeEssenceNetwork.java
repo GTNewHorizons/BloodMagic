b91ac930bafd7e1b42f5bd1d0707efdbebdf2b0d
FalseColor <florexiz.nm@gmail.com>
2026-05-07 10:51:23 +0300

    Save actual orb capacity instead of only tier

--- src/main/java/WayofTime/alchemicalWizardry/api/soulNetwork/LifeEssenceNetwork.java
+++ src/main/java/WayofTime/alchemicalWizardry/api/soulNetwork/LifeEssenceNetwork.java
@@ -5,23 +5,29 @@ import net.minecraft.nbt.NBTTagCompound;
 public class LifeEssenceNetwork extends net.minecraft.world.WorldSavedData {
 
     public int currentEssence;
+    public int maxEssence;
     public int maxOrb;
 
     public LifeEssenceNetwork(String par1Str) {
         super(par1Str);
         currentEssence = 0;
+        // can be lower than currentEssence (overfilled) with runes of the orb
+        maxEssence = 1;
         maxOrb = 0;
     }
 
     @Override
     public void readFromNBT(NBTTagCompound nbttagcompound) {
         currentEssence = nbttagcompound.getInteger("currentEssence");
+        maxEssence = nbttagcompound.hasKey("maxEssence") ? nbttagcompound.getInteger("maxEssence")
+                : SoulNetworkHandler.getMaximumForOrbTier(maxOrb); // seamless migration from old data
         maxOrb = nbttagcompound.getInteger("maxOrb");
     }
 
     @Override
     public void writeToNBT(NBTTagCompound nbttagcompound) {
         nbttagcompound.setInteger("currentEssence", currentEssence);
+        nbttagcompound.setInteger("maxEssence", maxEssence);
         nbttagcompound.setInteger("maxOrb", maxOrb);
     }
 }
