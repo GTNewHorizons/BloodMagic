ad1d128b94a4c9c1e7332cf41b64e2cf977fc4bc
koolkrafter5 <creepersrgreen@gmail.com>
2026-07-05 12:15:16 -0400

    Reimplement demon portal debug right-click for creative mode

--- src/main/java/WayofTime/alchemicalWizardry/common/demonVillage/tileEntity/TEDemonPortal.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/demonVillage/tileEntity/TEDemonPortal.java
@@ -945,23 +945,19 @@ public class TEDemonPortal extends TileEntity {
     }
 
     public void rightClickBlock(EntityPlayer player, int side) {
-        // if (worldObj.isRemote)
-        // {
-        // return;
-        // }
-        //
-        // this.initialize();
-        //
-        // if (ForgeDirection.getOrientation(side) == ForgeDirection.UP)
-        // {
-        // this.createRandomBuilding(DemonBuilding.BUILDING_HOUSE, 0);
-        // } else if (ForgeDirection.getOrientation(side) == ForgeDirection.DOWN)
-        // {
-        // this.createRandomBuilding(DemonBuilding.BUILDING_PORTAL, 0);
-        // } else
-        // {
-        // this.createRandomRoad();
-        // }
+        if (worldObj.isRemote || !player.capabilities.isCreativeMode) {
+            return;
+        }
+
+        this.initialize();
+
+        if (ForgeDirection.getOrientation(side) == ForgeDirection.UP) {
+            this.createRandomBuilding(DemonBuilding.BUILDING_HOUSE, 0);
+        } else if (ForgeDirection.getOrientation(side) == ForgeDirection.DOWN) {
+            this.createRandomBuilding(DemonBuilding.BUILDING_PORTAL, 0);
+        } else {
+            this.createRandomRoad();
+        }
     }
 
     public int createRandomBuilding(int type, int tier) {
